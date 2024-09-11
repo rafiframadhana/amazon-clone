7ae0d5eb2a264a980f832d4070783e01511e3674
rafiframadhana <rafiframadhana.cs@gmail.com>
2024-09-11 20:19:25 +0700

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,7 @@
 let productsHTML = '';
 
+
+
 products.forEach((product) => {
 
     productsHTML += `
@@ -79,8 +81,18 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
             });
         }
 
-        
 
+        let cartQuantity = 0;
+
+        cart.forEach((item)=>{
+            cartQuantity = cartQuantity + item.quantity;
+        })
+
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+        
+        console.log(cartQuantity);
         console.log(cart);
     })
-})
+});
+
+
