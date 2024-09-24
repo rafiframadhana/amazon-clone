c6d86dc7d735de085387a81b7fe6e50151d240d4
rafiframadhana <rafiframadhana.cs@gmail.com>
2024-09-24 15:49:50 +0700

    remove default test and add moneyTest

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -16,7 +16,7 @@ if(formatCurrency(0) === '0.00'){
     console.log('failed');
 }
 
-console.log('test rounding cents');
+console.log('rounds up to the nearest cent');
 if(formatCurrency(2000.5) === '20.01'){
     console.log('passed');
 }else{
