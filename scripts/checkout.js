f844ed2d4e3a1c6de2b3d39fa6655f1f2b19a794
rafiframadhana <rafiframadhana.cs@gmail.com>
2024-09-25 21:40:09 +0700

    Create OOP version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { renderCheckoutHeader } from './checkout/checkOutHeader.js';
+import '../data/cart-oop.js';
 
 renderOrderSummary();
 renderPaymentSummary();
